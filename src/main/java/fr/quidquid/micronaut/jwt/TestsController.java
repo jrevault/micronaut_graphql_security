d4818f90280a7a460e9eae10b551cbbec32c6912
jrevault <jrevault@gmail.com>
2020-12-02 08:46:38 +0100

    Simplified added logs

--- src/main/java/fr/quidquid/micronaut/jwt/TestsController.java
+++ src/main/java/fr/quidquid/micronaut/jwt/TestsController.java
@@ -3,6 +3,10 @@ package fr.quidquid.micronaut.jwt;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+
+import static io.micronaut.security.rules.SecurityRule.IS_ANONYMOUS;
+import static io.micronaut.security.rules.SecurityRule.IS_AUTHENTICATED;
 
 @Controller()
 public class TestsController {
@@ -11,11 +15,13 @@ public class TestsController {
     }
 
     @Get("/ping")
+    @Secured( IS_ANONYMOUS )
     public HttpResponse ping() {
         return HttpResponse.ok("pong");
     }
 
     @Get("/secret")
+    @Secured( IS_AUTHENTICATED )
     public HttpResponse secret() {
         return HttpResponse.ok("Access granted");
     }
